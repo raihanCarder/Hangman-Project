e4e304064d7d72dc983a6f8465af290d825481ee
Raihan Carder <339509226@GoTVDSB.ca>
2023-10-04 10:53:07 -0400

    Add project files.

---
+++ Hangman Project/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // Raihan Carder
+
+            List<string> wordBank = File.ReadAllLines("words.txt").ToList();
+            Random generator = new Random();
+            int randomWord;
+            bool done = false, game = false;
+
+            Console.Title = "Hangman Project";
+            Console.WriteLine("Welcome to Hangman!");
+            Console.WriteLine();
+            Console.WriteLine("To Play click Enter!");
+            Console.ReadLine();
+            Console.Clear();
+            while (!done)
+            {
+                randomWord = generator.Next(0, 854); // Creates new word that user must guess
+
+                while (!game)
+                {
+                    HangingMan(0);
+                    Console.ReadLine() ;
+                }                
+            }
+        }
+        public static void HangingMan(int guesses)
+        {
+            if (guesses == 0)
+            {
+                Console.WriteLine("____");
+                Console.WriteLine("|/   |");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|_____");
+            }
+        }
+    }
+}
+
---
+++ Hangman Project/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hangman Project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TVDSB")]
+[assembly: AssemblyProduct("Hangman Project")]
+[assembly: AssemblyCopyright("Copyright © TVDSB 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c78b27ac-510c-4dc6-bbd9-745ec965cbd8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
