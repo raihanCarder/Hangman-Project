e0dcbdb35b3a601b4ad637a6de2fd84326e31639
Raihan Carder <339509226@GoTVDSB.ca>
2023-10-05 11:02:30 -0400

    confusion

--- Hangman Project/Program.cs
+++ Hangman Project/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Hangman_Project
 {
@@ -15,8 +16,13 @@ namespace Hangman_Project
         {   // Raihan Carder
 
             List<string> wordBank = File.ReadAllLines("words.txt").ToList();
+            //List<char> lettersInWord = new List<char>();
+
+
             Random generator = new Random();
-            int randomWord;
+            string displayWord = "", guessedLetters = "", word;
+            char guess;
+            int randomWord, failedGuesses = 0;
             bool done = false, game = false;
 
             Console.Title = "Hangman Project";
@@ -25,15 +31,61 @@ namespace Hangman_Project
             Console.WriteLine("To Play click Enter!");
             Console.ReadLine();
             Console.Clear();
+
             while (!done)
             {
                 randomWord = generator.Next(0, 854); // Creates new word that user must guess
+                word = wordBank[randomWord].ToUpper();
 
-                while (!game)
+                for (int i = 0; i < word.Length; i++)
                 {
-                    HangingMan(0);
+                    displayWord += "_ ";
+                }
+
+                Console.WriteLine(wordBank[randomWord]); // delete later
+                Console.WriteLine(wordBank.Count); // delete later
+                                                                
+
+                while (!game)    // Starts game
+                {
+                    
+                    HangingMan(failedGuesses);
+                    Console.WriteLine();
+
+                    Console.WriteLine(displayWord);
+
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    //Console.WriteLine($"Guessed letters: {guessedLetters}");
+
+                    if (failedGuesses < 8)
+                    {
+                        Console.Write("Guess a letter: ");
+                       
+                        if (char.TryParse(Console.ReadLine().ToUpper().Trim(), out guess))
+                        {
+                            if (word.Contains(guess))
+                            {
+                                displayWord = displayWord.Remove(1,2);
+                                
+                            }
+                            else
+                            {
+                                failedGuesses += 1;
+                            }
+                        }
+                        Console.Clear();
+                                                 
+                    }
+                    else
+                    {
+
+                    }
+                    
+
+
                     Console.ReadLine() ;
-                }                
+                }
             }
         }
         public static void HangingMan(int guesses)
@@ -49,6 +101,88 @@ namespace Hangman_Project
                 Console.WriteLine("|   ");
                 Console.WriteLine("|_____");
             }
+            else if (guesses == 1)
+            {
+                Console.WriteLine("____");
+                Console.WriteLine("|/   |");
+                Console.WriteLine("|   (_)");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|_____");
+            }
+            else if (guesses == 2)
+            {
+                Console.WriteLine("____");
+                Console.WriteLine("|/   |");
+                Console.WriteLine("|   (_)");
+                Console.WriteLine("|    |");
+                Console.WriteLine("|    |");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|_____");
+            }
+            else if (guesses == 3)
+            {
+                Console.WriteLine("____");
+                Console.WriteLine("|/   |");
+                Console.WriteLine("|   (_)");
+                Console.WriteLine("|   \\|");
+                Console.WriteLine("|    |");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|_____");
+            }
+            else if (guesses == 4)
+            {
+                Console.WriteLine("____");
+                Console.WriteLine("|/   |");
+                Console.WriteLine("|   (_)");
+                Console.WriteLine("|   \\|/");
+                Console.WriteLine("|    |");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|_____");
+            }
+            else if (guesses == 5)
+            {
+                Console.WriteLine("____");
+                Console.WriteLine("|/   |");
+                Console.WriteLine("|   (_)");
+                Console.WriteLine("|   \\|/");
+                Console.WriteLine("|    |");
+                Console.WriteLine("|   /");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|_____");
+            }
+            else if (guesses == 6)
+            {
+                Console.WriteLine("____");
+                Console.WriteLine("|/   |");
+                Console.WriteLine("|   (_)");
+                Console.WriteLine("|   \\|/");
+                Console.WriteLine("|    |");
+                Console.WriteLine("|   / \\");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|_____");
+            }
+            else if (guesses == 7)
+            {
+                Console.WriteLine("____");
+                Console.WriteLine("|/   |");
+                Console.WriteLine("|   (_)");
+                Console.WriteLine("|   \\|/");
+                Console.WriteLine("|    |");
+                Console.WriteLine("|   | |");
+                Console.WriteLine("|   ");
+                Console.WriteLine("|_____");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+
         }
     }
 }
