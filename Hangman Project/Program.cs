90006ab0dff84ab662c289ee3eb092a878dfcc59
Raihan Carder <339509226@GoTVDSB.ca>
2023-10-11 10:33:47 -0400

    Almost done

--- Hangman Project/Program.cs
+++ Hangman Project/Program.cs
@@ -17,41 +17,60 @@ namespace Hangman_Project
         {   // Raihan Carder
 
             List<string> wordBank = File.ReadAllLines("words.txt").ToList();
+            List<string> seenWords = new List<string>();
             List<char> guessedLetters = new List<char>();
+            List<char> correctLetters = new List<char>();
             char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
 
             Random generator = new Random();
             string displayWord = "", word, playagain;
             char guess;
-            int randomWord, failedGuesses;
+            int randomWord, failedGuesses, wins = 0, losses = 0;
             bool done = false, game = false, validguess = false, gameWon = false, validAnswer = false;
 
             Console.Title = "Hangman Project";
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
             Console.WriteLine("Welcome to Hangman!");
             Console.WriteLine();
-            Console.WriteLine("To Play click Enter!");
+            Console.WriteLine("Click Enter To Play!");
             Console.ReadLine();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
             Console.Clear();
-
+            Console.WriteLine("How to play Hangman:");
+            Console.WriteLine();
+            Console.WriteLine("Guess a letter!");
+            Console.WriteLine("You get 7 tries to guess the word, run out of tries and you lose.");
+            Console.WriteLine("Your goal is to guess the word in under 7 tries.");
+            Console.Write("Understand? Click Enter to Play!");
+            Console.ReadLine();
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
             while (!done)
             {
                 failedGuesses = 0;
                 displayWord = "";
+                guessedLetters.Clear();
+                correctLetters.Clear();
 
                 randomWord = generator.Next(0, 854); // Creates new word that user must guess
                 word = wordBank[randomWord].ToUpper();
+
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Clear();
 
                 for (int i = 0; i < word.Length; i++)
                 {
                     displayWord += "_";                  
                 }
-
-                //Console.WriteLine(word); // delete later
-                //Console.WriteLine(displayWord.Count()); // Delete later
-                //Console.WriteLine(wordBank.Count); // delete later                                                              
-
+                                                              
                 while (!game)    // Starts game
-                {                  
+                {                   
                     HangingMan(failedGuesses);
                     Console.WriteLine();
                     Console.WriteLine();
@@ -70,12 +89,14 @@ namespace Hangman_Project
                     {
                         if (char.TryParse(Console.ReadLine().Trim().ToUpper(), out guess))
                         {
-                            if (alphabet.Contains(guess))
+                            if (alphabet.Contains(guess) && !correctLetters.Contains(guess) && !guessedLetters.Contains(guess))
                             {
                                 validguess = true;
 
                                 if (word.Contains(guess))
                                 {
+                                    correctLetters.Add(guess);
+
                                     for (int i = 0; i < word.Count(); i++)
                                     {                                     
                                         if (word[i] == guess)
@@ -101,26 +122,33 @@ namespace Hangman_Project
                             }
 
                         }
+                        else
+                        {
+                            Console.Write("Invalid, guess a letter: ");
+                        }    
                     }
                     validguess = false;
 
                     if (displayWord.Equals(word))
                     {
                         gameWon = true;
+                        wins++;
+                        seenWords.Add(word);
                     }
 
-                    if (failedGuesses == 7 || gameWon)
+                    if (failedGuesses == 8 || gameWon)
                     {
                         game = true;
+                        losses++;
                     }
                     else
                     {
                         Console.Clear();
 
-                        Console.Write("You have guessed: ");
+                        Console.Write("You have incorrectly guessed: ");
                         for (int i = 0; i < guessedLetters.Count(); i++)
                         {
-                            Console.Write(guessedLetters[i] + "");
+                            Console.Write(guessedLetters[i] + " ");
                         }
                         Console.WriteLine();
                     }
@@ -128,17 +156,20 @@ namespace Hangman_Project
                     
                 }
 
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.Clear();
                 Console.WriteLine("GAME OVER");
                 if (gameWon == true)
                 {
-                    Console.WriteLine("Congratulations you have guessed the word!");
+                    Console.WriteLine($"Congratulations you have correctly guessed the word: {word}!");
                 }
                 else
                 {
-                    Console.WriteLine("You've lost");
+                    Console.WriteLine($"You've lost, the Word was: {word}!");
                 }
-
+                Console.WriteLine($"You have won {wins} time(s).");
+                Console.WriteLine($"You have lost {losses} time(s).");
                 Console.Write("Wanna play again? (Yes/No) : ");
 
                 while (!validAnswer)
@@ -160,6 +191,7 @@ namespace Hangman_Project
                     else
                     {
                         Console.Write("Invalid Answer, Wanna play again? ");
+
                     }
                 }
                 validAnswer = false;
